89799da838ab700c938fff45b41a4b4e260dc8b8
machme <luasnk@gmail.com>
2018-04-11 16:09:26 +0900

    test3

--- src/main/java/examples/helloboard/dao/Test.java
+++ src/main/java/examples/helloboard/dao/Test.java
@@ -1,4 +1,13 @@
 package examples.helloboard.dao;
 
 public class Test {
+    String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
